7e87f775df444ac5c497368c73730e462c047ea5
iaace-NA <iaace-NA@users.noreply.github.com>
2018-01-04 04:12:10 -0500

    do not require time-ago for now

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -1,6 +1,6 @@
 "use strict";
 const Discord = require("discord.js");
-let ta = require("time-ago")();
+//let ta = require("time-ago")();
 const UTILS = new (require("../utils.js"))();
 module.exports = class EmbedGenrator {
 	constructor() { }
