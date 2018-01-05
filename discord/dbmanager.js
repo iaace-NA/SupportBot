cb6fa7bc60c904b0cc5b6830d4c7f6ba4593c883
iaace-NA <iaace-NA@users.noreply.github.com>
2018-01-05 02:11:09 -0500

    setup schemas

--- discord/dbmanager.js
+++ discord/dbmanager.js
@@ -2,11 +2,25 @@
 module.exports = class DBManager {//mongodb
 	constructor(CONFIG) {
 		this.namecache = require("mongoose");
-		this.namecache.connect("mongodb://localhost/namecache");
+		this.namecache.connect("mongodb://localhost/namecache");//cache of summoner object name lookups
 		this.namecache.on("error", function (e) { throw e; });
 		this.discordlink = require("mongoose");
-		this.discordlink.connect("mongodb://localhost/discordlink");
+		this.discordlink.connect("mongodb://localhost/discordlink");//links discord uids to namecache
 		this.discordlink.on("error", function (e) { throw e; });
+		this.userSchema = new this.namecache.Schema({
+			profileIconId: Number,
+			name: String,
+			summonerLevel: Number,
+			revisionDate: Number,//epoch time
+			id: Number,
+			accountId: Number,
+			lastUpdated: Number
+		});
+		let that = this;
+		this.linkSchema = new this.discordlink.Schema({
+			uid: String,
+			userref: that.discordlink.Schema.Types.ObjectId
+		});
 	}
 	test() {
 		let newEmbed = new Discord.RichEmbed();
