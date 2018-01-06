d16a2053fa5a8c6b1c52c30531baa145f1c4e2f6
iaace-NA <iaace-NA@users.noreply.github.com>
2018-01-06 04:58:40 -0500

    fix copy typo

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -34,7 +34,7 @@ module.exports = class EmbedGenrator {
 		}
 		let newEmbed = new Discord.RichEmbed();
 		newEmbed.setAuthor(summoner.name);
-		newEmbed.setThumbnail("https://ddragon.riotgames.com/cdn/" + CONFIG.STATIC.n.profileicon + "/img/profileicon/" + apiobj.profileIconId + ".png");
+		newEmbed.setThumbnail("https://ddragon.riotgames.com/cdn/" + CONFIG.STATIC.n.profileicon + "/img/profileicon/" + summoner.profileIconId + ".png");
 		newEmbed.setDescription("Level " + summoner.summonerLevel);
 		for (let b in ranks) {
 			newEmbed.addField(ranks[b].queueType + ": " + ranks[b].tier + " " + ranks[b].rank + " " + ranks[b].leaguePoints + "LP", (ranks[b].wins + ranks[b].losses) + "G = " + ranks[b].wins + "W + " + ranks[b].losses + "L\nWin Rate: " + UTILS.round(100 * ranks[b].wins / (ranks[b].wins + ranks[b].losses), 2) + "%");
