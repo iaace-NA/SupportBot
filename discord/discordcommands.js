b5071b8f76478c1916f91a89559d89952966f417
iaace-NA <iaace-NA@users.noreply.github.com>
2018-01-07 23:55:46 -0500

    make unlink command

--- discord/discordcommands.js
+++ discord/discordcommands.js
@@ -36,6 +36,9 @@ module.exports = function (CONFIG, client, lolapi, msg, db) {
 				db.addLink(msg.author.id, result).then(() => { reply("Your discord account is now linked to " + result.name); }).catch((e) => { reply("Something went wrong."); throw e; });
 			}).catch(console.error);
 		});
+		command([CONFIG.DISCORD_COMMAND_PREFIX + "unlink "], true, false, (original, index, parameter) => {
+			db.removeLink(msg.author.id).then(result => { reply(result); }).catch(e => { reply("An error has occurred."); throw e; });
+		});
 		command([CONFIG.DISCORD_COMMAND_PREFIX + "gl", CONFIG.DISCORD_COMMAND_PREFIX + "getlink"], false, false, (original, index, parameter) => {
 			db.getLink(msg.author.id).then(result => {
 				if (UTILS.exists(result)) reply("You're `" + result.name + "`");
