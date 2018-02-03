80d68844f98fb8a4ee8c87eaa6d6a9910bffe4cd
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-02-02 19:36:55 -0500

    syntax

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 "use strict";
-let ta = require("./timeago.js")();
+let ta = require("./timeago.js");
 module.exports = class UTILS {
 	output(t) {//general utility function
 		if (this.exists(t)) {
