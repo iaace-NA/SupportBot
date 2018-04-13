ec8037b09d68d396281ee6d434f340484ef32967
iaace-NA <iaace-NA@users.noreply.github.com>
2018-04-13 15:52:06 -0400

    fix reference error

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -143,7 +143,7 @@ module.exports = class EmbedGenrator {
 			let teams = {};
 			for (let b in matches[i].participants) {
 				if (!UTILS.exists(teams[matches[i].participants[b].teamId])) teams[matches[i].participants[b].teamId] = [];
-				if (matches[i].participants[b].player.summonerId !== summoner.id) teams[matches[i].participants[b].teamId].push(matches[i].participants[b]);
+				if (UTILS.findParticipantIdentityFromPID(matches[i], matches[i].participants[b].participantId).summonerId !== summoner.id) teams[matches[i].participants[b].teamId].push(matches[i].participants[b]);
 			}
 			for (let b in teams[teamParticipant.teamId]) {
 				const tmPI = UTILS.findParticipantIdentityFromPID(matches[i], teams[teamParticipant.teamId][b].participantId);
