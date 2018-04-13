a99efd6bc210fb0b6e74db5e445b4e3b1126bfd9
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-04-13 02:05:53 -0400

    typo

--- api/main.js
+++ api/main.js
@@ -22,7 +22,7 @@ const express = require("express");
 const website = express();
 const UTILS = new (require("../utils.js"))();
 UTILS.assert(UTILS.exists(CONFIG.API_PORT_PRODUCTION));
-UTILS.assert(UTILS.exists(CONFIG.API_PORT_DEVELOPOMENT));
+UTILS.assert(UTILS.exists(CONFIG.API_PORT_DEVELOPMENT));
 UTILS.output("Modules loaded.");
 ready();
 function ready() {
