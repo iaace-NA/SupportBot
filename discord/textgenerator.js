c0f83eb0604a5a8a76445ed2f8f5d9ba645a632f
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-04-13 02:45:26 -0400

    formatting

--- discord/textgenerator.js
+++ discord/textgenerator.js
@@ -6,6 +6,6 @@ module.exports = class TextGenerator {
 		nMsg.edit(nMsg.content + " " + (nMsg.createdTimestamp - msg.createdTimestamp) + "ms");
 	}
 	internal_ping(times) {
-		return "Time to internal api: " + (times.received - times.started) + " ms. Time to return: " + (times.ended - times.received);
+		return "Time to internal api: " + (times.received - times.started) + " ms. Time to return: " + (times.ended - times.received) + " ms.";
 	}
 }
