8c0378c794692310915f3021b6581159345b7451
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-04-20 18:33:10 -0400

    change format of no rank indicator

--- utils.js
+++ utils.js
@@ -150,7 +150,7 @@ module.exports = class UTILS {
 		//G2 +00 Gold 2, 0 LP
 		//G2 +56 Gold 2, 56LP
 		//G2P L_ Gold 2 promotion, 1 loss
-		if (!this.exists(info)) return "`██████`";
+		if (!this.exists(info)) return "`******`";
 		let answer = "`";
 		answer += info.tier.substring(0, 1);
 		if (this.exists(info.miniSeries)) {
