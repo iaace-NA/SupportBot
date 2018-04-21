c0ac5b8f17694a2b9cca550668b9e1a7cc66e73e
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-04-21 14:05:36 -0400

    round winrate to nearest integer for recent match history

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -202,12 +202,12 @@ module.exports = class EmbedGenerator {
 		for (let b in all_lanes_KDA) all_lanes_KDA[b].KDA =  (all_lanes_KDA[b].K + all_lanes_KDA[b].A) / all_lanes_KDA[b].D;
 		let lane_description = [];
 		for (let i = 0; i < 5; ++i) {
-			if (all_lanes[i] > 0) lane_description.push(CONFIG.EMOJIS.lanes[i] + " " + all_lanes[i] + "G (" + UTILS.round(100 * all_lanes_w[i] / (all_lanes_w[i] + all_lanes_l[i]), 2) + "%) = " + all_lanes_w[i] + "W + " + all_lanes_l[i] + "L\tKDA:`" + (UTILS.round(all_lanes_KDA[i].KDA, 2) == "Infinity" ? "Perfect" : UTILS.round(all_lanes_KDA[i].KDA, 2)) + "`");
+			if (all_lanes[i] > 0) lane_description.push(CONFIG.EMOJIS.lanes[i] + " " + all_lanes[i] + "G (" + UTILS.round(100 * all_lanes_w[i] / (all_lanes_w[i] + all_lanes_l[i]), 0) + "%) = " + all_lanes_w[i] + "W + " + all_lanes_l[i] + "L\tKDA:`" + (UTILS.round(all_lanes_KDA[i].KDA, 2) == "Infinity" ? "Perfect" : UTILS.round(all_lanes_KDA[i].KDA, 2)) + "`");
 		}
 		const total_wins = all_results.reduce((total, increment) => { return total + (increment ? 1 : 0); }, 0);
 		const total_losses = all_results.reduce((total, increment) => { return total + (increment ? 0 : 1); }, 0);
 		if (all_results.length > 5) newEmbed.addField("Older Match Results", all_results.slice(5).map(r => { return r ? CONFIG.EMOJIS.win : CONFIG.EMOJIS.loss; }).join("") + "->Oldest");
-		newEmbed.setDescription(all_results.length + "G (" +  UTILS.round(100 * total_wins / (total_wins + total_losses), 2) + "%) = " + total_wins + "W + " + total_losses + "L " + "\tKDA:`" + (UTILS.round(all_KDA.KDA, 2) == "Infinity" ? "Perfect" : UTILS.round(all_KDA.KDA, 2)) + "`\n" + lane_description.join("\n"));
+		newEmbed.setDescription(all_results.length + "G (" +  UTILS.round(100 * total_wins / (total_wins + total_losses), 0) + "%) = " + total_wins + "W + " + total_losses + "L " + "\tKDA:`" + (UTILS.round(all_KDA.KDA, 2) == "Infinity" ? "Perfect" : UTILS.round(all_KDA.KDA, 2)) + "`\n" + lane_description.join("\n"));
 		let rpw = [];//recently played with
 		for (let b in common_teammates) rpw.push([b, common_teammates[b].w, common_teammates[b].l]);
 		rpw.sort((a, b) => { return b[1] + b[2] - a[1] - a[2]; });
