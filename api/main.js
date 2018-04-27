4c058fb259f2258863637ef86ab210b05366451c
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-04-27 12:41:48 -0400

    irs maintenance function 10 min timeout

--- api/main.js
+++ api/main.js
@@ -73,6 +73,7 @@ function ready() {
 		});
 	});
 	serveWebRequest("/terminate_request/:request_id", function (req, res, next) {
+		for (let b in irs) if (new Date().getTime - irs[b][5] > 1000 * 60 * 10) delete irs[b];
 		if (!UTILS.exists(irs[req.params.request_id])) return res.status(200).end();
 		let description = [];
 		for (let i = 0; i < 5; ++i) description.push(response_type[i] + " (" + irs[req.params.request_id][i] + "):" + UTILS.round(100 * irs[req.params.request_id][i] / irs[req.params.request_id][0], 0) + "%");
