631235ca9a3a23f40f01b1c224d95cd71d105d1d
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-05-04 08:24:27 -0400

    fix indexing

--- api/main.js
+++ api/main.js
@@ -105,7 +105,7 @@ setInterval(() => {
 		if (!UTILS.exists(shard_ws_connected[i + ""])) shard_ws_connected[i + ""] = true;
 		if (!shard_ws_connected[i + ""]) shard_ws[i + ""] = undefined;
 	}
-	for (let b in shard_ws_connected) shard_ws_connected = false;
+	for (let b in shard_ws_connected) shard_ws_connected[b] = false;
 	shardBroadcast({ type: 0 });
 }, HEARTBEAT_INTERVAL);
 function allShardsConnected() {
