3212c2ec8da1ca0a45274567d69e6e013ede19a0
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-05-05 00:12:58 -0400

    fix KDA formatting, fixed length

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -501,7 +501,7 @@ module.exports = class EmbedGenerator {
 			let individual_description = "`";
 			individual_description += UTILS.shortRank(ranks[i].find(r => { return r.queueType === "RANKED_FLEX_SR"; })) + " ";
 			individual_description += UTILS.shortRank(ranks[i].find(r => { return r.queueType === "RANKED_FLEX_TT"; })) + " ";
-			individual_description += UTILS.shortRank(ranks[i].find(r => { return r.queueType === "RANKED_SOLO_5x5"; })) + "` ";
+			individual_description += UTILS.shortRank(ranks[i].find(r => { return r.queueType === "RANKED_SOLO_5x5"; })) + " ";
 			let results = [];
 			let all_KDA = {
 				K: 0,
@@ -520,11 +520,11 @@ module.exports = class EmbedGenerator {
 				if (streak_result == results[j]) streak_count++;
 				else break;
 			}
-			individual_description += streak_count + (streak_result ? "Ws " : "Ls ");//streak information
-			const total_wins = results.reduce((total, increment) => { return total + (increment ? 1 : 0); }, 0);
-			const total_losses = results.reduce((total, increment) => { return total + (increment ? 0 : 1); }, 0);
-			individual_description += total_wins + "W/" + total_losses + "L ";//20 game W/L record
-			individual_description += " @ `" + UTILS.KDAFormat((all_KDA.K + all_KDA.A) / all_KDA.D) + "` ";
+			individual_description += (streak_count + "").padStart(2, "0") + (streak_result ? "Ws " : "Ls ");//streak information
+			const total_wins = results.reduce((total, increment) => { return total + (increment ? 1 : 0); }, 0) + "";
+			const total_losses = results.reduce((total, increment) => { return total + (increment ? 0 : 1); }, 0) + "";
+			individual_description += total_wins.padStart(2, "0") + "W/" + total_losses.padStart(2, "0") + "L ";//20 game W/L record
+			individual_description += " @ " + UTILS.KDAFormat((all_KDA.K + all_KDA.A) / all_KDA.D) + "` ";
 			for (let j = 0; j < 3; ++j) {//top 3 champion masteries
 				individual_description += j < masteries[i].length ? CONFIG.STATIC.CHAMPIONS[masteries[i][j].championId].emoji : ":x:";
 			}
--- utils.js
+++ utils.js
@@ -191,7 +191,7 @@ module.exports = class UTILS {
 	}
 	KDAFormat(num) {
 		if (isNaN(num) || num == Infinity) return "Perfect";
-		else return this.round(num, 2);
+		else return this.round(num, 2).toFixed("2");
 	}
 	KPFormat(num) {
 		if (isNaN(num)) return 0;
