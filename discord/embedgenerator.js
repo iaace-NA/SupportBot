788110ce168cd19baf967c2b1ca51089de106183
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-05-04 22:22:57 -0400

    spaces get trimmed, alternative format given

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -429,7 +429,7 @@ module.exports = class EmbedGenerator {
 			}
 			UTILS.debug("team_description_c1 length: " + team_description_c1.length);
 			UTILS.debug("team_description_c2 length: " + team_description_c2.length);
-			newEmbed.addField(":x::x: `SOLOQ `¦`FLEX5 `¦`FLEX3 `", team_description_c1, true);
+			newEmbed.addField(":x::x: `SOLO Q FLEX 5 FLEX 3`", team_description_c1, true);
 			newEmbed.addField("Bans: " + ban_description.join(""), team_description_c2, true);
 			++team_count;
 		}
