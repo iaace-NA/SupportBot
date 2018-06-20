3f8d972172f0725cc2fe1d4fde635d94ada6c8a4
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-06-19 23:46:50 -0400

    add utility function for conditional formatting

--- utils.js
+++ utils.js
@@ -332,4 +332,7 @@ module.exports = class UTILS {
 	map(x, in_min, in_max, out_min, out_max) {
 		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 	}
+	conditionalFormat(text, surrounds, condition = true) {
+		return condition ? surrounds + text + surrounds : text;
+	}
 }
