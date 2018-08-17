b3a4f07b7eacfab8db2aa5c254a22b2ea43b0fcf
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-08-17 03:58:43 -0400

    debug print

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -625,7 +625,7 @@ module.exports = class EmbedGenerator {
 
 		let team_by_all_ranks = [];//array of stats objects
 		for (let b in TEAM_COMBINATIONS) team_by_all_ranks.push(UTILS.calculateTeamStatistics(mathjs, TEAM_COMBINATIONS[b], ranks.map(r => UTILS.averageUserMMR(r))));
-		UTILS.debug(JSON.stringify(team_by_all_ranks, null, "\t"));
+		UTILS.debug(JSON.stringify(ranks.map(r => UTILS.averageUserMMR(r)), null, "\t"));
 		const team_by_all_ranks_lowest_diff = mathjs.min(team_by_all_ranks.map(t => t.abs));
 		UTILS.debug("rank lowest diff is " + team_by_all_ranks_lowest_diff);
 		const team_by_all_ranks_best = team_by_all_ranks.findIndex(t => t.abs === team_by_all_ranks_lowest_diff);//team arrangement index
