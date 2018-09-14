cd68425e0c9ea6a7afe17b9c185f3e551241341a
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-09-14 15:52:21 -0400

    limit display of mastery

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -941,8 +941,9 @@ module.exports = class EmbedGenerator {
 		const SECTION_LENGTH = 15;
 		if (cm_description.length > 0) {
 			const sections = Math.trunc(cm_description.length / SECTION_LENGTH) + 1;
-			for (let i = 0; i < sections; ++i) newEmbed.addField("Individual Champion Stats:", cm_description.slice(i * SECTION_LENGTH, (i + 1) * SECTION_LENGTH).join("\n"), true);
+			for (let i = 0; i < sections && i < 4; ++i) newEmbed.addField("Individual Champion Stats:", cm_description.slice(i * SECTION_LENGTH, (i + 1) * SECTION_LENGTH).join("\n"), true);
 		}
+		newEmbed.setFooter("Showing a maximum of 60 champions");
 		return newEmbed;
 	}
 }
