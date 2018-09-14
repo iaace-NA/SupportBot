58b0c9c5d632443f6641e0ce99bcaa4dd1979c56
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-09-14 08:43:49 -0400

    fix ref error

--- discord/textgenerator.js
+++ discord/textgenerator.js
@@ -17,7 +17,7 @@ module.exports = class TextGenerator {
 	}
 	owners(CONFIG) {
 		let answer = "Here are my bot owners:\n`user id:name`";
-		for (let b in CONFIG.OWNER_DISCORD_IDS) if (CONFIG.OWNER_DISCORD_IDS.active) answer += "\n`" + b + ":" + CONFIG.OWNER_DISCORD_IDS[b].name + "`";
+		for (let b in CONFIG.OWNER_DISCORD_IDS) if (CONFIG.OWNER_DISCORD_IDS[b].active) answer += "\n`" + b + ":" + CONFIG.OWNER_DISCORD_IDS[b].name + "`";
 		return answer;
 	}
 }
