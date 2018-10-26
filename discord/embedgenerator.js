3a9405dab38fc37aba142e459dd7596a3e591094
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-10-26 08:32:43 -0400

    fix ftg formatting for TT

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -871,7 +871,7 @@ module.exports = class EmbedGenerator {
 			}
 			for (let i = 0; i < team_by_tt_ranks_description.length; ++i) {
 				team_by_tt_ranks_description[i].sort((a, b) => b[0] - a[0]);
-				team_by_tt_ranks_description[i] = "**__Team " + sideOfMap(team_by_tt_ranks[team_by_tt_ranks_best].diff, i) + team_by_tt_ranks_description[i].map(d => d[1]).join("\n") + "\n" + formatDescriptionStringRanks(team_by_tt_ranks[team_by_tt_ranks_best], i);
+				team_by_tt_ranks_description[i] = "**__Team " + sideOfMap(team_by_tt_ranks[team_by_tt_ranks_best].diff, i) + "__**\n" + team_by_tt_ranks_description[i].map(d => d[1]).join("\n") + "\n" + formatDescriptionStringRanks(team_by_tt_ranks[team_by_tt_ranks_best], i);
 				team_by_tt_ranks_description[i] = team_by_tt_ranks_description[i].trim();
 			}
 			newEmbed.addField("By Skill", team_by_tt_ranks_description[0], true);
