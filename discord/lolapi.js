d5a1bc1c2563fb8336cbbcc84457686f535fda01
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2018-12-05 18:28:31 -0500

    fix ref error

--- discord/lolapi.js
+++ discord/lolapi.js
@@ -139,8 +139,8 @@ module.exports = class LOLAPI {
 	getStaticChampions(region, version, locale = "en_US") {
 		UTILS.debug("STATIC CHAMPIONS: " + region);
 		return new Promise((resolve, reject) => {
-			let v = UTILS.exists(version) ? version : realm.v;
 			this.getStatic("realms/" + this.CONFIG.REGIONS_REVERSE[region].toLowerCase() + ".json").then(realm => {
+				let v = UTILS.exists(version) ? version : realm.v;
 				this.getStatic("cdn/" + v + "/data/" + locale + "/champion.json").then(cd => {//champion data
 					for (let b in cd.data) {
 						cd.data[cd.data[b].key] = cd.data[b];//add key as duplicate of data
@@ -154,8 +154,8 @@ module.exports = class LOLAPI {
 	getStaticSummonerSpells(region, version, locale = "en_US") {
 		UTILS.output("STATIC SPELLS: " + region);
 		return new Promise((resolve, reject) => {
-			let v = UTILS.exists(version) ? version : realm.v;
 			this.getStatic("realms/" + this.CONFIG.REGIONS_REVERSE[region].toLowerCase() + ".json").then(realm => {
+				let v = UTILS.exists(version) ? version : realm.v;
 				this.getStatic("cdn/" + v + "/data/" + locale + "/summoner.json").then(sd => {//spell data
 					for (let b in sd.data) {
 						sd.data[sd.data[b].key] = sd.data[b];//add key as duplicate of data
