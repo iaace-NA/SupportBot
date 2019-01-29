e6d43d3ed19b1d4c5c3455461a9090456c24db4d
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-01-29 06:56:47 -0500

    remove args array

--- utils/utils.js
+++ utils/utils.js
@@ -709,10 +709,10 @@ module.exports = class UTILS {
 	gnuPlotGoldAdvantageGraph(array_of_points, x_size = 50, y_size = 18) {//[{x, y}, ...]
 		let that = this;
 		return new Promise((resolve, reject) => {
-			const wincmd = "-Command \"\\\"" + array_of_points.map(p => p.x + " " + p.y).join("`n") + "\\\" | gnuplot -e \\\"set terminal dumb " + x_size + " " + y_size + "; set xlabel 'Minutes'; set tics scale 0; plot '-' with filledcurves y=0 notitle\\\"\"";
+			const wincmd = "powershell.exe -Command \"\\\"" + array_of_points.map(p => p.x + " " + p.y).join("`n") + "\\\" | gnuplot -e \\\"set terminal dumb " + x_size + " " + y_size + "; set xlabel 'Minutes'; set tics scale 0; plot '-' with filledcurves y=0 notitle\\\"\"";
 			const linuxcmd = "printf \"" + array_of_points.map(p => p.x + " " + p.y).join("\\n") + "\" | gnuplot -e \"set terminal dumb " + x_size + " " + y_size + "; set xlabel 'Minutes'; set tics scale 0; plot '-' with filledcurves y=0 notitle\"";
 			if (process.platform === "win32") {
-				child_process.exec("powershell.exe", [wincmd], { timeout: 1000, shell: "powershell.exe" }, (err, stdout, stderr) => {
+				child_process.exec(wincmd, { timeout: 1000, shell: "powershell.exe" }, (err, stdout, stderr) => {
 					if (err) reject(err);
 					if (that.exists(stderr) && stderr != "") reject(stderr);
 					else resolve(stdout);
