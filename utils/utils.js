57bcf1c97bc618a8ea0cf4c7b524d98fbac1bce2
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-02-24 17:08:48 -0500

    add permute function to UTILS

--- utils/utils.js
+++ utils/utils.js
@@ -755,4 +755,22 @@ module.exports = class UTILS {
 			}
 		});
 	}
+	permute(ext_input) {
+		let permArr = [];
+		let usedChars = [];
+		internal_permute(ext_input);
+		function internal_permute(input) {
+			for (let i = 0; i < input.length; i++) {
+				let ch = input.splice(i, 1)[0];
+				usedChars.push(ch);
+				if (input.length == 0) {
+				permArr.push(usedChars.slice());
+				}
+				internal_permute(input);
+				input.splice(i, 0, ch);
+				usedChars.pop();
+			}
+		}
+		return permArr;
+	};
 }
