a98559bccd9f5bfbd440cfb3c0aff00925076dba
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-05-03 15:58:26 -0400

    fix ref error

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -484,7 +484,8 @@ module.exports = class EmbedGenerator {
 			let premade_game = false;
 			for (let b in teams[teamParticipant.teamId]) {
 				const teamPID = UTILS.findParticipantIdentityFromPID(matches[i], teams[teamParticipant.teamId][b].participantId);
-				if (UTILS.exists(common_teammates[teamPID.summonerName]) && common_teammates[teamPID.summonerName].w + common_teammates[teamPID.summonerName].l > 1) {
+				const candidate = common_teammates.find(ct => ct[0] === teamPID.summonerName);
+				if (UTILS.exists(candidate) && candidate.w + candidate.l > 1) {
 					premade_game = true;
 					break;
 				}
