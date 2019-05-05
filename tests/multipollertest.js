25d4a8939fb5ac6bcb9f8956bfcf40c791b3d0d4
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-05-05 18:11:06 -0400

    finish writing multipoller test

--- tests/multipollertest.js
+++ tests/multipollertest.js
@@ -2,22 +2,45 @@
 let UTILS = require("../utils/utils.js");
 let MultiPoller = require("../utils/multipoller.js");
 let sample_db = {};
-for (let i = 0; i < 50; ++i) {
+const sample_size = 15;
+for (let i = 0; i < sample_size; ++i) {
 	sample_db[i + ""] = UTILS.now() + UTILS.randomInt(1000, 60000);
 }
-let test_system = new MultiPoller("Test");
+printDB();
+let test_system = new MultiPoller("Test", updatesDue, checkForUpdates, checkReadyForUpdate, justUpdated, () => {}, {
+	min_queue_length: 0,
+	max_queue_length: 100,
+	slow_update_interval: 2000,
+	fast_update_interval: 100,
+	status_check_interval: 10000
+});
 function updatesDue() {
-	let ans = [];//{id: "", options: {}, order: 0}
-	for (let i = 0; i < 50; ++i) {
-		if (sample_db[i + ""] < UTILS.now()) ans.push({ id: i + "", options: {}, order: sample_db[i + ""] });
-	}
-	ans.sort((a, b) => a.order - b.order);
-	return ans;
+	return new Promise((resolve, reject) => {
+		let ans = [];//{id: "", options: {}, order: 0}
+		for (let i = 0; i < sample_size; ++i) {
+			if (sample_db[i + ""] < UTILS.now()) ans.push({ id: i + "", options: {}, order: sample_db[i + ""] });
+		}
+		ans.sort((a, b) => a.order - b.order);
+		resolve(ans);
+	});
 }
 function checkForUpdates(id, options) {
 	return new Promise((resolve, reject) => {
-		sample_db[id] = UTILS.now() + UTILS.randomInt(1000, 60000);
-		resolve();
+		const new_time = UTILS.now() + UTILS.randomInt(1000, 60000);
+		UTILS.output("updating ID: " + id + ". age (ms): " + UTILS.now() - sample_db[id]);
+		sample_db[id] = new_time;
+		resolve(new_time);
 	});
 }
-function checkReadyForUpdate(id)
+function checkReadyForUpdate(id) {
+	return new Promise((resolve, reject) => {
+		resolve(sample_db[i + ""] < UTILS.now());
+	});
+}
+function justUpdated(id, results, error) {
+	UTILS.output("ID: " + id + " was just updated with result: " + results + " . error: " + error);
+	printDB();
+}
+function printDB() {
+	sample_db[i + ""] = UTILS.now() + UTILS.randomInt(1000, 60000);
+}
