6b487fcfeb1bad52371b4b37d0ab1a5dc28efda0
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2022-02-13 13:42:01 -0800

    Update embedgenerator.js

--- discord/embedgenerator.js
+++ discord/embedgenerator.js
@@ -295,7 +295,12 @@ function getLikelyLanes(CONFIG, champion_ids, smites) {
 	let answer = {};
 	answer.assignments = lane_permutations[index_of_max].map(lane_number => lane_number + 1);
 	answer.confidence = max / 5;
-	UTILS.output("highest probability lane assignments are:\n" + answer.assignments.map((lane_number, index) => CONFIG.STATIC.CHAMPIONS[champion_ids[index]].name + ": " + ["Top", "Jungle", "Mid", "Support", "Bot"][lane_number - 1] + " : " + LANE_PCT[champion_ids[index]][lane_number - 1] + "%").join("\n") + "\nwith total probability: " + (max / 5) + "%");
+	let debug_message = "highest probability lane assignments are:\n" + answer.assignments.map((lane_number, index) => {
+		if (!LANE_PCT[champion_ids[index]]) return CONFIG.STATIC.CHAMPIONS[champion_ids[index]].name + ": ";
+		return CONFIG.STATIC.CHAMPIONS[champion_ids[index]].name + ": " + ["Top", "Jungle", "Mid", "Support", "Bot"][lane_number - 1] + " : " + LANE_PCT[champion_ids[index]][lane_number - 1] + "%";
+	}
+	).join("\n") + "\nwith total probability: " + (max / 5) + "%";
+	UTILS.output(debug_message);
 	return answer;
 }
 module.exports = class EmbedGenerator {
